f96f1f883eb5425067b5fe9f5660d53319755f71
DeepikaGM <60542337+DeepikaGM@users.noreply.github.com>
2023-10-31 16:48:23 +0530

    Update scrapper_2.py

--- scrapper_2.py
+++ scrapper_2.py
@@ -1,117 +1,38 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import time
-import pandas as pd
+from bs4 import BeautifulSoup as bs
 import requests
-import csv
+import pandas as pd
 
-# NASA Exoplanet URL
-START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
 
-# Webdriver
-browser = webdriver.Edge("C:/Whitehat_jr/PRO-127-130/msedgedriver.exe")
-browser.get(START_URL)
+page = requests.get(url)
 
-time.sleep(10)
+soup = bs(page.text,'html.parser')
 
-planets_data = []
+star_table = soup.find_all('table')
 
-headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date", "hyperlink", 
-            "planet_type", "planet_radius", "orbital_radius", "orbital_period", "eccentricity"]
+temp_list= []
 
-def scrape():
-    for i in range(1,5):
-        while True:
-            time.sleep(2)
+table_rows = star_table[7].find_all('tr')
 
-            soup = BeautifulSoup(browser.page_source, "html.parser")
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
 
-            # Check page number    
-            current_page_num = int(soup.find_all("input", attrs={"class", "page_num"})[0].get("value"))
-
-            if current_page_num < i:
-                browser.find_element(By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-            elif current_page_num > i:
-                browser.find_element(By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[1]/a').click()
-            else:
-                break
-
-        for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
-            li_tags = ul_tag.find_all("li")
-            temp_list = []
-            for index, li_tag in enumerate(li_tags):
-                if index == 0:
-                    temp_list.append(li_tag.find_all("a")[0].contents[0])
-                else:
-                    try:
-                        temp_list.append(li_tag.contents[0])
-                    except:
-                        temp_list.append("")
-
-            # Get Hyperlink Tag
-            hyperlink_li_tag = li_tags[0]
-
-            temp_list.append("https://exoplanets.nasa.gov"+ hyperlink_li_tag.find_all("a", href=True)[0]["href"])
-            
-            planets_data.append(temp_list)
-
-        browser.find_element(By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-
-        print(f"Page {i} scraping completed")
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
 
 
-# Calling Method
-scrape()
+for i in range(1,len(temp_list)):
+    
+    Star_names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])
 
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
+print(df2)
 
-new_planets_data = []
-url="https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
-def scrape_more_data(hyperlink):
-    try:
-        page = requests.get(url)
-      
-        soup = BeautifulSoup(page.content, "html.parser")
-        star_table=soup.find_all('table')
-
-        table_rows=star_table[7].find_all('tr')
-
-        
-
-        temp_list = []
-
-        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
-            td_tags = tr_tag.find_all("td")
-          
-            for td_tag in td_tags:
-                try: 
-                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
-                except:
-                    temp_list.append("")
-                    
-        new_planets_data.append(temp_list)
-
-    except:
-        time.sleep(1)
-        scrape_more_data(hyperlink)
-
-#Calling method
-
-for index, data in enumerate(planets_data):
-    scrape_more_data(data[5])
-    print(f"scraping at hyperlink {index+1} is completed.")
-
-print(new_planets_data[0:10])
-
-final_planet_data = []
-
-for index, data in enumerate(planets_data):
-    new_planet_data_element = new_planets_data[index]
-    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
-    new_planet_data_element = new_planet_data_element[:7]
-    final_planet_data.append(data + new_planet_data_element)
-
-with open("final.csv", "w") as f:
-        csvwriter = csv.writer(f)
-        csvwriter.writerow(headers)
-        csvwriter.writerows(final_planet_data)
+df2.to_csv('dwarf_stars.csv')
